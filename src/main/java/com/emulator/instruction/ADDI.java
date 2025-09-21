d425a35dc0e505d117826a82079bd93f508b5178
hvsilva <hvsilva@hotmail.com>
2025-09-21 19:38:04 -0300

    -Implementação de api

---
+++ src/main/java/com/emulator/instruction/ADDI.java
@@ -0,0 +1,214 @@
+package com.emulator.instruction;
+
+import com.emulator.CPU68000;
+import com.emulator.GenInstruction;
+import com.emulator.Size;
+
+//NAME
+//ADDI -- Add immediate
+//
+//SYNOPSIS
+//ADDI	#<data>,<ea>
+//
+//Size = (Byte, Word, Long)
+//
+//FUNCTION
+//Adds the immediate data to the destination operand, and
+//stores the result in the destination location. The size of the
+//operation may be specified as byte, word, or long. The size of the
+//immediate data matches the operation size.
+//
+//FORMAT
+//                                                      <ea>
+//----------------------------------------=========================
+//|15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+//|---|---|---|---|---|---|---|---|-------|-----------|-----------|
+//| 0 | 0 | 0 | 0 | 0 | 1 | 1 | 0 | SIZE  |   MODE    |  REGISTER |
+//|---------------------------------------------------------------|
+//| 16 BITS DATA (with last Byte) |          8 BITS DATA          |
+//|---------------------------------------------------------------|
+//|             32 BITS DATA (included last Word)                 |
+//-----------------------------------------------------------------
+//
+//SIZE
+//00->one Byte operation
+//01->one Word operation
+//10->one Long operation
+//
+//REGISTER
+//<ea> is always destination, addressing modes are the followings:
+//
+//--------------------------------- -------------------------------
+//|Addressing Mode|Mode| Register | |Addressing Mode|Mode|Register|
+//|-------------------------------| |-----------------------------|
+//|      Dn       |000 |N� reg. Dn| |    Abs.W      |111 |  000   |
+//|-------------------------------| |-----------------------------|
+//|      An       | -  |     -    | |    Abs.L      |111 |  001   |
+//|-------------------------------| |-----------------------------|
+//|     (An)      |010 |N� reg. An| |   (d16,PC)    | -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|     (An)+     |011 |N� reg. An| |   (d8,PC,Xi)  | -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|    -(An)      |100 |N� reg. An| |   (bd,PC,Xi)  | -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|    (d16,An)   |101 |N� reg. An| |([bd,PC,Xi],od)| -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|   (d8,An,Xi)  |110 |N� reg. An| |([bd,PC],Xi,od)| -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|   (bd,An,Xi)  |110 |N� reg. An| |    #data      | -  |   -    |
+//|-------------------------------| -------------------------------
+//|([bd,An,Xi]od) |110 |N� reg. An|
+//|-------------------------------|
+//|([bd,An],Xi,od)|110 |N� reg. An|
+//---------------------------------
+//
+//RESULT
+//X - Set the same as the carry bit.
+//N - Set if the result is negative. Cleared otherwise.
+//Z - Set if the result is zero. Cleared otherwise.
+//V - Set if an overflow is generated. Cleared otherwise.
+//C - Set if a carry is generated. Cleared otherwise.
+public class ADDI implements GenInstructionHandler {
+
+	final CPU68000 cpu;
+	
+	public ADDI(CPU68000 cpu) {
+		this.cpu = cpu;
+	}
+
+	@Override
+	public void generate() {
+		int base = 0x0600;
+		GenInstruction ins;
+		
+		for (int s = 0; s < 3; s++) {
+			if (s == 0b00) {
+				ins = new GenInstruction() {
+					
+					@Override
+					public void run(int opcode) {
+						ADDIByte(opcode);
+					}
+
+				};
+			} else if (s == 0b01) {
+				ins = new GenInstruction() {
+					
+					@Override
+					public void run(int opcode) {
+						ADDIWord(opcode);
+					}
+
+				};
+			} else {
+				ins = new GenInstruction() {
+					
+					@Override
+					public void run(int opcode) {
+						ADDILong(opcode);
+					}
+
+				};
+			}
+		
+			for (int m = 0; m < 8; m++) {
+				if (m == 1) {
+					continue;
+				}
+				
+				for (int r = 0; r < 8; r++) {
+					if ((m == 7) && r > 0b001) {
+						continue;
+					}
+					
+					int opcode = base | (s << 6) | (m << 3) | r;
+					cpu.addInstruction(opcode, ins);
+				}
+			}
+		}
+		
+	}
+	
+	private void ADDIByte(int opcode) {
+		int mode = (opcode >> 3) & 0x7;
+		int register = (opcode & 0x7);
+	
+		long data = cpu.bus.read(cpu.PC + 2, Size.WORD);
+		data = data & 0xFF;
+			 
+		cpu.PC += 2;
+			 
+		Operation o = cpu.resolveAddressingMode(Size.BYTE, mode, register);
+		long toAdd = o.getAddressingMode().getByte(o);
+		
+		long tot = toAdd + data;
+		cpu.writeKnownAddressingMode(o, tot, Size.BYTE);
+		
+		calcFlags(tot, data, toAdd, Size.BYTE.getMsb(), Size.BYTE.getMax());
+	}
+
+	private void ADDIWord(int opcode) {
+		int mode = (opcode >> 3) & 0x7;
+		int register = (opcode & 0x7);
+		
+		long data = cpu.bus.read(cpu.PC + 2, Size.WORD);
+		
+	 	cpu.PC += 2;
+		
+		Operation o = cpu.resolveAddressingMode(Size.WORD, mode, register);
+		long toAdd = o.getAddressingMode().getWord(o);
+		
+		long tot = toAdd + data;
+		cpu.writeKnownAddressingMode(o, tot, Size.WORD);
+		
+		calcFlags(tot, data, toAdd, Size.WORD.getMsb(), Size.WORD.getMax());
+	}
+	
+	private void ADDILong(int opcode) {
+		int mode = (opcode >> 3) & 0x7;
+		int register = (opcode & 0x7);
+
+		long data = cpu.bus.read(cpu.PC + 2, Size.LONG);
+		
+	 	cpu.PC += 4;
+		
+		Operation o = cpu.resolveAddressingMode(Size.LONG, mode, register);
+		long toAdd = o.getAddressingMode().getLong(o);
+		
+		long tot = toAdd + data;
+		cpu.writeKnownAddressingMode(o, tot, Size.LONG);
+		
+		calcFlags(tot, data, toAdd, Size.LONG.getMsb(), Size.LONG.getMax());
+	}
+	
+	void calcFlags(long tot, long data, long toAdd, long msb, long maxSize) {
+		if ((tot & maxSize) == 0) {
+			cpu.setZ();
+		} else {
+			cpu.clearZ();
+		}
+		if ((tot & msb) > 0) {
+			cpu.setN();
+		} else {
+			cpu.clearN();
+		}
+		
+		boolean Dm = (data & msb) > 0;
+		boolean Sm = (toAdd & msb) > 0;
+		boolean Rm = (tot & msb) > 0;
+		if((Sm && Dm && !Rm) || (!Sm && !Dm && Rm)) {
+			cpu.setV();
+		} else {
+			cpu.clearV();
+		}
+		
+		if (tot > maxSize) {
+			cpu.setC();
+			cpu.setX();
+		} else {
+			cpu.clearC();
+			cpu.clearX();
+		}
+	}
+	
+}
---
+++ src/main/java/com/emulator/instruction/ASL.java
@@ -0,0 +1,350 @@
+package com.emulator.instruction;
+
+import com.emulator.CPU68000;
+import com.emulator.GenInstruction;
+import com.emulator.Size;
+
+
+//NAME
+//ASL, ASR -- Arithmetic shift left and arithmetic shift right
+//
+//SYNOPSIS
+//ASd	Dx,Dy
+//ASd	#<data>,Dy
+//ASd	<ea>
+//where d is direction, L or R
+//
+//Size = (Byte, Word, Long)
+//
+//FUNCTION
+//Performs an arithmetic shifting bit operation in the indicated
+//    direction, with an immediate data, or with a data register.
+//    If you shift address contents, you only can do ONE shift, and
+//    your operand is ONE word exclusively.
+//
+//ASL:              <--  
+//      C <------ OPERAND <--- 0
+//            |
+//            |
+//      X <---'
+//       
+//
+//ASR:      -->
+//  .---> OPERAND ------> C
+//      |    T          |
+//  |    |          |
+//  `----'          `---> X
+//
+//FORMAT
+//In the case of the shifting of a register:
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//-----------------------------------------------------------------
+//|15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+//|---|---|---|---|-----------|---|-------|---|---|---|-----------|
+//| 1 | 1 | 1 | 0 |  NUMBER/  |dr |  SIZE |i/r| 0 | 0 | REGISTER  |
+//|   |   |   |   |  REGISTER |   |       |   |   |   |           |
+//-----------------------------------------------------------------
+//
+//In the case of the shifting of a memory area:
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//-----------------------------------------------------------------
+//|15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+//|---|---|---|---|---|---|---|---|---|---|-----------|-----------|
+//| 1 | 1 | 1 | 0 | 0 | 0 | 0 |dr | 1 | 1 |    MODE   | REGISTER  |
+//----------------------------------------=========================
+//                                                  <ea>
+//
+//
+//NUMBER/REGISTER
+//Specifies number of shifting or number of register which contents
+//the number of shifting.
+//If i/r = 0, number of shifting is specified in the instruction as
+//immediate data
+//If i/r = 1, it's specified in the data register.
+//If dr = 0, right shifting
+//If dr = 1, left shifting
+//
+//SIZE
+//00->one Byte operation
+//01->one Word operation
+//10->one Long operation
+//
+//REGISTER
+//For a register shifting:
+//Indicates the number of data register on which shifting is applied.
+//
+//For a memory shifting:
+//<ea> indicates operand which should be shifted.
+//Only addressing modes relatives to memory are allowed:
+//
+//--------------------------------- -------------------------------
+//|Addressing Mode|Mode| Register | |Addressing Mode|Mode|Register|
+//|-------------------------------| |-----------------------------|
+//|      Dn       | -  |     -    | |    Abs.W      |111 |  000   |
+//|-------------------------------| |-----------------------------|
+//|      An       | -  |     -    | |    Abs.L      |111 |  001   |
+//|-------------------------------| |-----------------------------|
+//|     (An)      |010 |N� reg. An| |   (d16,PC)    | -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|     (An)+     |011 |N� reg. An| |   (d8,PC,Xi)  | -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|    -(An)      |100 |N� reg. An| |   (bd,PC,Xi)  | -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|    (d16,An)   |101 |N� reg. An| |([bd,PC,Xi],od)| -  |   -    | 
+//|-------------------------------| |-----------------------------|
+//|   (d8,An,Xi)  |110 |N� reg. An| |([bd,PC],Xi,od)| -  |   -    |
+//|-------------------------------| |-----------------------------|
+//|   (bd,An,Xi)  |110 |N� reg. An| |    #data      | -  |   -    |
+//|-------------------------------| -------------------------------
+//|([bd,An,Xi]od) |110 |N� reg. An|
+//|-------------------------------|
+//|([bd,An],Xi,od)|110 |N� reg. An|
+//---------------------------------
+//
+//RESULT
+//X - Set according to the last bit shifted out of the operand.
+//    Unaffected for a shift count of zero.
+//N - Set if the most-significant bit of the result is set. Cleared
+//    otherwise.
+//Z - Set if the result is zero. Cleared otherwise.
+//V - Set if the most significant bit is changed at any time during
+//    the shift operation. Cleared otherwise.
+//C - Set according to the last bit shifted out of the operand.
+//    Cleared for a shift count of zero.
+public class ASL implements GenInstructionHandler {
+
+	final CPU68000 cpu;
+	
+	public ASL(CPU68000 cpu) {
+		this.cpu = cpu;
+	}
+	
+	@Override
+	public void generate() {
+		generateImmediateRegister();
+		generateMemory();
+	}
+
+	private void generateImmediateRegister() {
+		int base = 0xE100;
+		GenInstruction ins = null;
+		
+		for (int s = 0; s < 3; s++) {
+			if (s == 0b00) {
+				ins = new GenInstruction() {
+					@Override
+					public void run(int opcode) {
+						ASLByte(opcode);
+					}
+				};
+			} else if (s == 0b01) {
+				ins = new GenInstruction() {
+					@Override
+					public void run(int opcode) {
+						ASLWord(opcode);
+					}
+				};
+			} else if (s == 0b10) {
+				ins = new GenInstruction() {
+					@Override
+					public void run(int opcode) {
+						ASLLong(opcode);
+					}
+				};
+			}
+					
+			for (int ir = 0; ir < 2; ir++) {
+				for (int r = 0; r < 8; r++) {
+					for (int nReg = 0; nReg < 8; nReg++) {
+						int opcode = base | (nReg << 9) | (s << 6) | (ir << 5) | r;
+						cpu.addInstruction(opcode, ins);
+					}
+				}
+			}
+		}
+	}
+	
+	private void generateMemory() {
+		int base = 0xE1C0;
+		GenInstruction ins = null;
+		
+		ins = new GenInstruction() {
+			@Override
+			public void run(int opcode) {
+				ASLMemoryWord(opcode);
+			}
+		};
+		for (int m = 0; m < 8; m++) {
+			if (m == 0 || m == 1) {
+				continue;
+			}
+			for (int r = 0; r < 8; r++) {
+				if (m == 0b111 && r > 0b001) {
+					continue;
+				}
+				int opcode = base | (m << 3) | r;
+				cpu.addInstruction(opcode, ins);
+			}
+		}
+	}
+	
+	private void ASLByte(int opcode) {
+		int register = (opcode & 0x7);
+		boolean ir = cpu.bitTest(opcode, 5);
+		int numRegister = (opcode >> 9) & 0x7;
+		
+		long shift;
+		if (!ir) {
+			if (numRegister == 0) {
+				shift = 8;
+			} else {
+				shift = numRegister;
+			}
+		} else {
+			shift = cpu.getDLong(numRegister);
+			shift = shift & 63;
+		}
+		
+		int data = (int) (cpu.getDByte(register));
+		
+		int msb;
+		int last_out = 0;
+		int msb_changed = 0;
+		for(int s= 0; s < shift; s++) {
+			last_out = data & 0x80;
+			data <<= 1;
+			msb = data & 0x80;
+			if (msb != last_out) {
+				msb_changed = 1;
+			}
+		}
+		data &= 0xFF;
+		
+		cpu.setDByte(register, data);
+		
+		calcFlags(data, shift, msb_changed, last_out, Size.BYTE.getMsb());
+	}
+	
+	private void ASLWord(int opcode) {
+		int register = (opcode & 0x7);
+		boolean ir = cpu.bitTest(opcode, 5);
+		int numRegister = (opcode >> 9) & 0x7;
+		
+		long shift;
+		if (!ir) {
+			if (numRegister == 0) {
+				shift = 8;
+			} else {
+				shift = numRegister;
+			}
+		} else {
+			shift = cpu.getDLong(numRegister);
+			shift = shift & 63;
+		}
+		
+		int data = (int) (cpu.getDWord(register));
+		
+		int msb;
+		int last_out = 0;
+		int msb_changed = 0;
+		for(int s= 0; s < shift; s++) {
+			last_out = data & 0x8000;
+			data <<= 1;
+			msb = data & 0x8000;
+			if (msb != last_out) {
+				msb_changed = 1;
+			}
+		}
+		data &= 0xFFFF;
+		
+		cpu.setDWord(register, data);
+		
+		calcFlags(data, shift, msb_changed, last_out, Size.WORD.getMsb());
+	}
+	
+	private void ASLLong(int opcode) {
+		int register = (opcode & 0x7);
+		boolean ir = cpu.bitTest(opcode, 5);
+		int numRegister = (opcode >> 9) & 0x7;
+		
+		long shift;
+		if (!ir) {
+			if (numRegister == 0) {
+				shift = 8;
+			} else {
+				shift = numRegister;
+			}
+		} else {
+			shift = cpu.getDLong(numRegister);
+			shift = shift & 63;
+		}
+		
+		long data = cpu.getDLong(register);
+		
+		long msb;
+		long last_out = 0;
+		long msb_changed = 0;
+		for(int s= 0; s < shift; s++) {
+			last_out = data & 0x8000_0000;
+			data <<= 1;
+			msb = data & 0x8000_0000;
+			if (msb != last_out) {
+				msb_changed = 1;
+			}
+		}
+		
+		cpu.setDLong(register, data);
+					
+		calcFlags(data, shift, msb_changed, last_out, Size.LONG.getMsb());
+	}
+	
+	private void ASLMemoryWord(int opcode) {
+		int register = (opcode & 0x7);
+		int mode = (opcode >> 3) & 0x7;
+		
+		Operation o = cpu.resolveAddressingMode(Size.WORD, mode, register);
+		long data = o.getData() & 0xFFFF;
+		int last_out = (int) (data & 0x8000);
+		int msb_changed = 0;
+
+		data <<= 1;
+		if ((data & 0x8000) != last_out) {
+			msb_changed = 1;
+		}
+		
+		cpu.setDWord(register, data);
+					
+		calcFlags(data, 1, msb_changed, last_out, Size.WORD.getMsb());
+	}
+	
+	private void calcFlags(long data, long shift, long msb_changed, long last_out, long msb) {
+		if (data == 0) {
+			cpu.setZ();
+		} else {
+			cpu.clearZ();
+		}
+		
+		if ((data & msb) > 0) {
+			cpu.setN();
+		} else {
+			cpu.clearN();
+		}
+		
+		if (msb_changed != 0) {
+			cpu.setV();
+		} else {
+			cpu.clearV();
+		}
+		
+		if (shift != 0) {
+			if (last_out != 0) {
+				cpu.setC();
+				cpu.setX();
+			} else {
+				cpu.clearC();
+				cpu.clearX();
+			}
+		}
+	}
+	
+}
