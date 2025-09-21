2ee474f8652d327d0e878c0d676505b535048cd4
hvsilva <hvsilva@hotmail.com>
2025-09-21 19:55:21 -0300

    -Add instruções

---
+++ src/main/java/com/emulator/instruction/RTE.java
@@ -0,0 +1,70 @@
+package com.emulator.instruction;
+
+import com.emulator.CPU68000;
+import com.emulator.GenInstruction;
+import com.emulator.Size;
+
+public class RTE implements GenInstructionHandler {
+
+	final CPU68000 cpu;
+	
+	public RTE(CPU68000 cpu) {
+		this.cpu = cpu;
+	}
+	
+//	NAME
+//	RTE -- Return from exception (PRIVILEGED)
+//
+//SYNOPSIS
+//	RTE
+//
+//FUNCTION
+//	SR and PC are restored by SP. All SR bits are affected.
+//
+//FORMAT
+//	-----------------------------------------------------------------
+//	|15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+//	|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|
+//	| 0 | 1 | 0 | 0 | 1 | 1 | 1 | 0 | 0 | 1 | 1 | 1 | 0 | 0 | 1 | 1 |
+//	-----------------------------------------------------------------
+//
+//RESULT
+//	SR is set following to the restored word taken from SP.
+	
+	@Override
+	public void generate() {
+		int base = 0x4E73;
+		GenInstruction ins;
+		
+		ins = new GenInstruction() {
+			
+			@Override
+			public void run(int opcode) {
+				RTEpc(opcode);
+			}
+		};
+		
+		cpu.addInstruction(base, ins);
+	}
+	
+	private void RTEpc(int opcode) {
+		long SR = cpu.bus.read(cpu.SSP, Size.WORD);
+		cpu.SSP += 2;
+		
+		cpu.SR = (int) SR;
+		
+		long newPC;
+		newPC = cpu.bus.read(cpu.SSP, Size.LONG);
+		cpu.SSP += 4;
+		
+		cpu.PC = newPC - 2;
+		
+		if ((cpu.SR & 0x2000) == 0x2000) {
+			cpu.setALong(7, cpu.SSP);
+		} else {
+			cpu.setALong(7, cpu.USP);
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/emulator/instruction/RTR.java
@@ -0,0 +1,85 @@
+package com.emulator.instruction;
+
+import com.emulator.CPU68000;
+import com.emulator.GenInstruction;
+import com.emulator.Size;
+
+public class RTR implements GenInstructionHandler {
+
+	final CPU68000 cpu;
+	
+	public RTR(CPU68000 cpu) {
+		this.cpu = cpu;
+	}
+	
+//	NAME
+//	RTR -- Return and restore condition code register
+//
+//SYNOPSIS
+//	RTR
+//
+//FUNCTION
+//	CCR and PC are restored by SP.
+//	Supervisor byte of SR isn't affected.
+//
+//FORMAT
+//	-----------------------------------------------------------------
+//	|15 |14 |13 |12 |11 |10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+//	|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|
+//	| 0 | 1 | 0 | 0 | 1 | 1 | 1 | 0 | 0 | 1 | 1 | 1 | 0 | 1 | 1 | 1 |
+//	-----------------------------------------------------------------
+//
+//RESULT
+//	CCR is set following to the restored word taken from SP.
+	
+	@Override
+	public void generate() {
+		int base = 0x4E77;
+		GenInstruction ins;
+		
+		ins = new GenInstruction() {
+			
+			@Override
+			public void run(int opcode) {
+				RTRpc(opcode);
+			}
+		};
+		
+		cpu.addInstruction(base, ins);
+	}
+	
+	private void RTRpc(int opcode) {
+		long newPC;
+		long newSR;
+		
+		if ((cpu.SR & 0x2000) == 0x2000) {
+			newSR = cpu.bus.read(cpu.SSP, Size.WORD);
+			cpu.SSP += 2;
+			
+			int flags = (int) (newSR & 0x1F);	// solo se usa el byte inferior con los 5 flags
+			cpu.SR = (int) ((cpu.SR & 0xFFE0) | flags);
+			
+			newPC = cpu.bus.read(cpu.SSP, Size.LONG);
+			cpu.SSP += 4;
+			
+			cpu.setALong(7, cpu.SSP);
+			
+			cpu.PC = newPC - 2;
+		} else {
+			newSR = cpu.bus.read(cpu.USP, Size.WORD);
+			cpu.USP += 2;
+			
+			int flags = (int) (newSR & 0x1F);	// solo se usa el byte inferior con los 5 flags
+			cpu.SR = (int) ((cpu.SR & 0xFFE0) | flags);
+			
+			newPC = cpu.bus.read(cpu.USP, Size.LONG);
+			cpu.USP += 4;
+			
+			cpu.setALong(7, cpu.USP);
+			
+			cpu.PC = newPC - 2;
+		}
+		
+	}
+
+}
